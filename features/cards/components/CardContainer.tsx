3afebf82e35e18f1f937bc20841a1151bb77da78
Alejandro Montes <alejandro.montes.g@gmail.com>
2026-04-27 20:59:00 +0200

    feat(cards): add not-found pages and handle missing cards

--- features/cards/components/CardContainer.tsx
+++ features/cards/components/CardContainer.tsx
@@ -1,10 +1,11 @@
 import { getCardById } from "@cards/services/cards";
 import Card from "@cards/components/Card";
+import { notFound } from "next/navigation";
 
 export async function CardContainer(props: { id: string }) {
   const card = await getCardById(props.id);
   if (!card) {
-    return; // TODO Make card not found component
+    return notFound();
   }
 
   return <Card {...card} />;
